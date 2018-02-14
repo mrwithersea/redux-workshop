a7a78c7d6ef34e3e4e7bff7cc4e0c95e0d47c053
Marc Burton <marc.burton@wealthwizards.com>
2018-02-14 08:52:39 +0000

    wip

--- sample/selectors/ramda/selectors.js
+++ sample/selectors/ramda/selectors.js
@@ -2,6 +2,8 @@ import R from 'ramda';
 
 export const getSimpleValue = R.converge(
   R.sum,
-  R.pathOr(0, ['form', 'data', 'value']),
-  R.pathOr(0, ['datastore', 'response', 'value'])
+  [
+    R.pathOr(0, ['form', 'data', 'value']),
+    R.pathOr(0, ['datastore', 'response', 'value']),
+  ]
 );
